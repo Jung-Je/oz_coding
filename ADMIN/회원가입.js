d5226423b8b0948b08d026d6306ed46a2a51e3b7
jung <jjlee030415@gmail.com>
2024-10-10 18:00:18 +0900

    수정본

--- ADMIN/회원가입.js
+++ ADMIN/회원가입.js
@@ -4,11 +4,29 @@ form.addEventListener("submit", function(event){
     event.preventDefault() // 기존 기능 차단
 
     let userId = event.target.id.value
+    let userPw1 = event.target.pw1.value
+    let userPw2 = event.target.pw2.value
     let userName = event.target.name.value
     let userEmail = event.target.email.value
+    let userGender = event.target.gender.value
+    let userPhone = event.target.phone.value
+
+    if(userId.length < 6){
+        alert("아이디가 너무 짧습니다. 6자 이상 입력해주세요.")
+        return
+    }
+
+    if(userPw1 !== userPw2){
+        alert("비밀번호가 일치하지 않습니다.")
+        return
+    }
 
     alert(`환영합니다.
     아이디 : ${userId}
-    이메일 : ${userEmail}`)
+    이메일 : ${userEmail}
+    이름 : ${userName}
+    전화번호 : ${userPhone}
+    성별 : ${userGender}
+    `)
 
 })
