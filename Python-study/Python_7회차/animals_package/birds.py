ca8aa49c79f1d0ee57ee650da0da3de9d4e33e75
jung <jjlee030415@gmail.com>
2024-10-22 15:29:30 +0900

    Python_7회차

---
+++ Python-study/Python_7회차/animals_package/birds.py
@@ -0,0 +1,6 @@
+class Eagle:
+    def __init__(self, name):
+        self.name = name
+
+    def info(self):
+        return f"Eagle: {self.name}"
---
+++ Python-study/Python_7회차/animals_package/mammals.py
@@ -0,0 +1,6 @@
+class Dog:
+    def __init__(self, name):
+        self.name = name
+
+    def info(self):
+        return f"Dog: {self.name}" 
