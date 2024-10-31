6640e7d2983dd71d7ad90f467736006faced6970
Jung-Je <jjlee030415@gmail.com>
2024-11-01 00:16:51 +0900

    ✨Feat : DataBase 3일차

---
+++ DataBase-study/chapter14_21.sql
@@ -0,0 +1,29 @@
+ USE testdatabase;
+
+-- INSERT INTO employees (name, position, salary) VALUES
+-- 	('혜린', 'PM', 90000),
+--     ('은우', 'Frontend', 80000),
+--     ('가을', 'Backend', 92000),
+--     ('지수', 'Frontend', 78000),
+--     ('민혁', 'Frontend', 96000),
+--     ('하온', 'Backend', 130000);
+
+-- SELECT name, salary FROM employees;
+
+-- SELECT * FROM employees WHERE salary BETWEEN 0 AND 90000;
+
+-- SET SQL_SAFE_UPDATES = 0; 
+
+-- UPDATE employees
+-- SET salary = salary * 1.1
+-- WHERE position = 'PM';
+
+-- SELECT * FROM employees WHERE position = 'PM';
+
+-- UPDATE employees
+-- SET salary = salary * 1.05
+-- WHERE position = 'Backend';
+
+-- DELETE FROM employees WHERE name = '민혁';
+
+-- DELETE FROM employees;
